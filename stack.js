e82c6e446fbd6263abdeba4726e390551401df9b
programmingArrow <nishant.kashyap2104@gmail.com>
2019-11-22 23:27:00 +0530

    Completed Stack implementation

--- stack.js
+++ stack.js
@@ -1,4 +1,32 @@
-function stack()
-{
-    //test function
+
+var Stack = function() {
+    this.count = 0;
+    this.storage = {};
+
+    // Adds the value at the end of the stack
+    this.push = function (value) {
+        this.storage[this.count] = value;
+        this.count++;
+    }
+
+    // Removes and returns the value at the end of the stack
+    this.pop = function () {
+        if (this.count === 0){
+            return undefined;
+        }
+
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+    this.size = function () {
+        return this.count;
+    }
+
+    // Returns the value at the end of the stack
+    this.peek = function() {
+        return this.storage[this.count-1];
+    }
 }
