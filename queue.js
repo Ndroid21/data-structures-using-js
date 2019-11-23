78769fc1b3390a1b006d9298b18196a61a961e11
programmingArrow <nishant.kashyap2104@gmail.com>
2019-11-23 23:17:16 +0530

    implement queue data structure

---
+++ queue.js
@@ -0,0 +1,26 @@
+function Queue () {
+    collection = [];
+    this.print = function () {
+        console.log(collection);
+    }
+
+    this.enqueue = function (element) {
+        collection.push(element);
+    }
+
+    this.dequeue = function (element) {
+        return collection.shift();
+    }
+
+    this.front = function () {
+        return collection[0];
+    }
+
+    this.size = function () {
+        return collection.length;
+    }
+
+    this.isEmpty = function () {
+        return (collection === 0);
+    }
+};
