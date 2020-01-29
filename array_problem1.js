e8364ef0dfce39cbcddee24c235ed400540e5497
programmingArrow <nishant.kashyap2104@gmail.com>
2020-01-29 23:25:40 +0530

    Array Problem 1

---
+++ array_problem1.js
@@ -0,0 +1,25 @@
+// Given [4, 6, 2, 5, 9, 7, 3, 1, 8]
+
+// produce result as (1, 9), (2, 8), (3, 7), (4, 6), 5 
+// result can be without parenthesis ()
+
+const givenArray = [4, 6, 2, 5, 9, 7, 3, 1, 8];
+
+function arrayProblem1(givenArray) {
+    // sort array in asc order
+
+    // divide the sorted array into 2 different arrays
+
+    // sort the second array in desc order
+
+    // matching 0th key of first array to 0th key of second array and so on
+
+}
+
+function sortArrayAsc(arrayToSort = []){
+    // sort array in asc order
+}
+
+function sortArrayDesc(arrayToSort = []) {
+    // sort array in desc order
+}
